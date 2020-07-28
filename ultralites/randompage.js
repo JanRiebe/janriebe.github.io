bc05dcdab0a13b3420b084c3fe52181458864b60
Jan Riebe <corone.et.monedula@gmail.com>
2020-07-28 08:18:13 +0200

    Adding story hawk in the morning mist.

---
+++ ultralites/randompage.js
@@ -0,0 +1,13 @@
+
+var links = new Array();
+links[0] = "loremipsum.html";
+links[1] = "someother.html";
+links[2] = "hawk_in_the_morning_mist.html";
+
+function openLink() {
+  // Chooses a random link:
+  var i = Math.floor(Math.random() * links.length);
+  // Directs the browser to the chosen target:
+  parent.location = links[i];
+  return false;
+}
